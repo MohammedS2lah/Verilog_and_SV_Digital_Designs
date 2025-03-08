c55a7fcdf2fd0126da2f768c25425952a71add6e
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-03-08 02:22:53 +0200

    Update absolute_subtraction.v

--- Verilog/1 - Absolute Subtraction/absolute_subtraction.v
+++ Verilog/1 - Absolute Subtraction/absolute_subtraction.v
@@ -11,7 +11,7 @@ module absolute_subtraction(
     begin
       res = i_absolute_subtraction_A - i_absolute_subtraction_B;
       
-	  if(res[3])
+      if(res[3])
         o_absolute_subtraction_value = i_absolute_subtraction_B - i_absolute_subtraction_A;
       else
         o_absolute_subtraction_value = res;
