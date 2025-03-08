2d5b758da6059344baabe5d24b3b9df9a0294750
Mohammed Salah Aboshosha <95502907+MohammedS2lah@users.noreply.github.com>
2025-03-08 02:21:57 +0200

    Create absolute_subtraction.v

---
+++ Verilog/1 - Absolute Subtraction/absolute_subtraction.v
@@ -0,0 +1,20 @@
+module absolute_subtraction(
+  input wire signed [3:0] i_absolute_subtraction_A, 
+  input wire signed [3:0] i_absolute_subtraction_B, 
+
+  output reg signed [3:0] o_absolute_subtraction_value
+);
+
+  reg  [3:0] res;
+
+  always @(*)
+    begin
+      res = i_absolute_subtraction_A - i_absolute_subtraction_B;
+      
+	  if(res[3])
+        o_absolute_subtraction_value = i_absolute_subtraction_B - i_absolute_subtraction_A;
+      else
+        o_absolute_subtraction_value = res;
+    end
+
+endmodule
